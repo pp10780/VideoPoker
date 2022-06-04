8e882cfeb8a7b778a880e4c2909c712b384a8cc1
pp10780 <64440643+pp10780@users.noreply.github.com>
2022-06-04 23:42:09 +0100

    Add files via upload

---
+++ Java - Video Poker/src/Player/Hand.java
@@ -0,0 +1,16 @@
+package Player;
+
+import Card.*;
+
+public class Hand {
+
+	/**
+	 * 
+	 * @param hand
+	 */
+	public Hand(Card[] hand) {
+		// TODO - implement Hand.Hand
+		throw new UnsupportedOperationException();
+	}
+
+}
